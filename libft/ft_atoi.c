b2c8b5cb2121a2c910aa94d29319a3e8385ad09a
Elliot BERTIN <ebertin@e2r9p7.42.fr>
2017-11-12 17:51:53 +0100

    Finish patch non bonus

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,36 +6,36 @@
 /*   By: ebertin <ebertin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 09:43:04 by ebertin           #+#    #+#             */
-/*   Updated: 2017/11/10 15:57:07 by ebertin          ###   ########.fr       */
+/*   Updated: 2017/11/12 17:43:44 by ebertin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *str)
+int		ft_atoi(char const *str)
 {
 	int i;
-	int neg;
-	int result;
+	int d;
+	int p;
 
-	result = 0;
 	i = 0;
-	neg = 1;
-	while (str[i] == 32 || str[i] == '\t' || str[i] == '\v' || str[i] == '\f'
-		|| str[i] == '\r' || str[i] == '\n')
+	p = 0;
+	d = 0;
+	while (str[i] == '\n' || str[i] == '\t' || str[i] == '\v' || \
+			str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
 		i++;
-	if (str[i] == '-')
+	if (str[i] == '-' || str[i] == '+')
 	{
-		neg = -1;
+		if (str[i] == '-')
+			p = 1;
 		i++;
 	}
-	while (str[i])
+	while ((str[i] >= '0' && str[i] <= '9'))
 	{
-		if (!(str[i] >= 48 && str[i] <= 57))
-			return (result * neg);
-		else
-			result = result * 10 + str[i] - 48;
+		d = d * 10 + str[i] - 48;
 		i++;
 	}
-	return (result * neg);
+	if (p == 1)
+		d = d * -1;
+	return (d);
 }
